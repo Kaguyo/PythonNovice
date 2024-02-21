73a999b447654b44b39754fa358ed9a439563e45
Gabriel <111669913+Kaguyo@users.noreply.github.com>
2024-02-21 13:05:36 -0300

    Finished Functions2

--- Functions2.py
+++ Functions2.py
@@ -48,119 +48,151 @@ print("Peso_Municao_Rifle_Oca", Single_Rifle_Hollow_Point_Bullet_Weight, "kg")
 
 
 
-
-
 # Equipamentos Municiados/Detalhados Peso
-Rifle = 4
-Revolver = 1.3
+Recurve_Bow = 1
 
-Rifle_Loaded = 4.1397
-Rifle_Loaded_Hollow_Tip = 4.0092 
+Rifle = 4.13
 
-Revolver_Loaded = 1.3064
-Revolver_Loaded_Hollow_Tip = 1.3061
+Revolver = 1.4
+
+Pistol = 1.2
 
 Municao762 = 0.0097
 Municao762_Hollow = 0.0092
 
-Municao45ACP = 0.064
-Municao45ACP_Hollow = 0.061
+Municao45ACP = 0.0064
+Municao45ACP_Hollow = 0.0061
+
+Bow_Arrows = 0.025
 
 Crowbar = 2.2
 
 Knife = 0.4
 
+Motorcycle = 80
 
 Backpack = 0.15
-"Boosts 10kg capacity"
+"Allows to carry 1 more Long Weapon, up to 5kg of Basic Supplements plus 2 Small Itens and 1 Medium Item or Weapon"
 
 Basic_Supplements = 0
 
+
+# Categorias de Itens
+Small_Itens = { 
+    Knife, Municao45ACP, Municao45ACP_Hollow, Municao762, Municao762_Hollow
+    }
+Medium_Itens = {
+    Crowbar, Backpack
+}
+Large_Itens = { 
+    Motorcycle
+}
+
+Long_Weapons = { 
+    Rifle
+}
+
+Medium_Weapons = {
+    
+}
+
+Small_Weapons = {
+    Revolver, Pistol
+}
+
+Special_Weapons = {
+    Recurve_Bow
+}
 
 # Personagem
 Nome = "Ellie" 
 Ellie = {
 "Height": (166, "cm"),
 "Age": (18, "cm"),
-"Weight": (58, "kg"),
-}
-
-# Arma de fogo em posse:
-"Rifle"
-"Revolver"
-
-# Municao de armas de fogo em posse:
-
-# Numero 1 = Ponta oca.
-# Numero 2 = comum.
-Revolver_Hollow_Ammo_Type =()
-Revolver_Ammo_Type = True
-
-Rifle_Hollow_Ammo_Type = False
-Rifle_Ammo_Type =()
-
+} 
+Weight = 58
 
 # Capacidade Rifle: 1-8
 # Capacidade Revolver 1-6
 # Disparos Restantes
 Rifle_Rounds = 7
 Revolver_Rounds = 3
+Recurve_Bow_Arrows_Left = 0
+Pistol_Rounds = 0
 
+# Corpo Personagem
+Hands = Rifle + Municao762_Hollow*Rifle_Rounds
 
+Back = Crowbar + Backpack + Municao45ACP*9
 
-# Equipamento no Corpo
-Hands= (Rifle + Municao762_Hollow*8) 
+Belt = Revolver + Municao45ACP*Revolver_Rounds
 
-Back= (Crowbar, Backpack)
+Garter_Belt = Knife
 
-Belt= (Revolver + Municao45ACP*9)
+Total_Weight = Weight + Back + Belt + Garter_Belt + Hands
 
-Garter_Belt= (Knife)
+# Item em mãos
+Empunhando = Rifle
 
-Total_Weight = 58 + Crowbar + Rifle + Municao762_Hollow*Rifle_Rounds + Backpack + Revolver + Municao45ACP*Revolver_Rounds + Knife + Municao762_Hollow*8 + Municao45ACP*9
 print("Informacoes do personagem:")
-print("Carga limite", 82, "kg", "(+10kg Backpack) ")
-print("Carga atual", Total_Weight, "kg")
-
-
+print("Peso de", Nome, Weight,":", "kg")
+CargaLimite = Weight * 1.4655172413793104
+print("Carga Limite:", CargaLimite, "kg")
+if Total_Weight <= 85 : 
+    print("Carga atual:", Total_Weight, "kg")
+if Total_Weight > 85 :
+    print("Carga Limite atingida (",CargaLimite," kg) Desempenho será afetado.")
 
 
 # Um Trilhao de variaveis com codigos importantes
-Rifleloaded = True if Rifle_Rounds == 1 or (2,3,4,5,6,7,8) else()
-Rifleloaded_Hollow_Point = True if Rifle_Rounds == 1 or (2,3,4,5,6,7,8) else()
-
-Revolverloaded = True if Revolver_Rounds == 1 or (2,3,4,5,6,7,8) else()
-Revolverloaded_Hollow_Point = True if Revolver_Rounds == 1 or (2,3,4,5,6,7,8) else()
-
-
-if Rifleloaded : Rifle + 0.13
-if Rifleloaded_Hollow_Point : Rifleloaded_Hollow_Point + Single_Rifle_Hollow_Point_Bullet_Weight
 
 
 
-# Tambor Revolver
+# Empunhar Item X
+if Empunhando == Knife:
+    print("Empunhando: Faca")
+if Empunhando == Rifle:
+    print("Empunhando: Rifle (", Rifle_Rounds, ")")
+if Empunhando == Revolver:
+    print("Empunhando: Revolver (",Revolver_Rounds, ")")
+if Empunhando == Recurve_Bow:
+    print("Empunhando: Arco Recurvo (", Recurve_Bow_Arrows_Left, ")")
+if Empunhando == Crowbar:
+    print("Empunhando: Pé de Cabra")
+if Empunhando == Pistol:
+    print("Empunhando: Pistol (", Pistol_Rounds, ")")
+
+# Revolver_Chamber
 Revolver_Rounds_Upperlimit = 6
 Revolver_Rounds_Lowerlimit = 1
-if Revolver_Rounds > Revolver_Rounds_Upperlimit:
+if Revolver_Rounds > Revolver_Rounds_Upperlimit and Empunhando == Revolver:
     print("Revolver Rounds: Invalid")
-if Revolver_Rounds < Revolver_Rounds_Lowerlimit:
-    print("Revolver Rounds: Unloaded")
+if Revolver_Rounds < Revolver_Rounds_Lowerlimit and Empunhando == Revolver:
+    print("Revolver: Unloaded")
 
 # Rifle_Maganize
-Rifle_Magazine_Weight = 0.13
 Rifle_Rounds_Upperlimit = 8
 Rifle_Rounds_Lowerlimit = 1
-if Rifle_Rounds > Rifle_Rounds_Upperlimit:
+
+if Rifle_Rounds > Rifle_Rounds_Upperlimit and Empunhando == Rifle:
     print("Rifle Rounds: Invalid")
-if Rifle_Rounds < Rifle_Rounds_Lowerlimit:
-    print("Rifle Rounds: Unloaded")
+if Rifle_Rounds < Rifle_Rounds_Lowerlimit and Empunhando == Rifle:
+    print("Rifle: Unloaded")
 
-if Rifle > 4 : Rifle + 0.13
-if Revolver > 1.3 : Revolver == Revolver_Loaded
+# Pistol_Magazine 
+Pistol_Rounds_Upperlimit = 15
+Pistol_Rounds_Lowerlimit = 1
 
+if Pistol_Rounds > Pistol_Rounds_Upperlimit and Empunhando == Pistol:
+    print("Pistol Rounds: Invalid")
+if Pistol_Rounds < Pistol_Rounds_Lowerlimit and Empunhando == Pistol:
+    print("Pistol: Unloaded")
 
-if Revolver_Ammo_Type : Revolver_Hollow_Ammo_Type = False
-if Revolver_Hollow_Ammo_Type : Revolver_Ammo_Type = False
+# Recurve_Bow_Arrows
+Recurve_Bow_Rounds_Upperlimit = 1
+Recurve_Bow_Rounds_Lowerlimit = 1
 
-if Rifle_Ammo_Type : Rifle_Hollow_Ammo_Type = False
-if Rifle_Hollow_Ammo_Type : Rifle_Ammo_Type = False
+if Recurve_Bow_Arrows_Left > Recurve_Bow_Rounds_Upperlimit and Empunhando == Recurve_Bow:
+    print("Nocking Arrow: Invalid")
+if Recurve_Bow_Arrows_Left < Recurve_Bow_Rounds_Lowerlimit and Empunhando == Recurve_Bow:
+    print("Bow: Unloaded")
