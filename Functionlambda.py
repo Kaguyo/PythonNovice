6ad03144a17a722eef3fc58d0dca4184f5047814
Gabriel <111669913+Kaguyo@users.noreply.github.com>
2024-03-02 21:28:29 -0300

    hj foi só erro de python e django.

---
+++ Functionlambda.py
@@ -0,0 +1,8 @@
+# Lambda Functions:
+# Lambda functions, also known as anonymous functions, are small, inline functions that can have 
+# any number of arguments but can only have one expression. They are defined using the lambda keyword.
+
+double = lambda x: x * 2
+print(double(3))
+
+
