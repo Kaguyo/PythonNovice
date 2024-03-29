76be129cf7c70f2d251e2fa3469346e85d3c0eba
Gabriel <111669913+Kaguyo@users.noreply.github.com>
2024-03-29 17:29:23 -0300

    portas

--- Logic-Gate.py
+++ Logic-Gate.py
@@ -1,14 +1,46 @@
 # Logic gate AND
 
+# Obs: O Python identifica '0' como falso, portanto o código pode apresentar falhas se as portas de entrada
+# estiverem com o valor '0' por uma confusão na comunicação. O computador entende apenas 0 e 1, porem para representar o zero
+# usaremos 1 na hora de atribuir valor para a função, e 2 para  representar verdadeiro(1).
+
 # Composto por uma porta lógica, com simbolo GENERALIZADO similar a um D. Qual terá valores de entrada 
 # indicando valor de 1 ou 0, quando todos forem iguais, terá resultado de saída positivo (1) por ex:
 
 
-
-def logic_gate_and_pass (x):
+def logic_gate_and(x):
     return x, x, x
 
-Entrada_A, Entrada_B, Entrada_C = logic_gate_and_pass(0)
-if Entrada_A == Entrada_B and Entrada_C: 
+Entrada_A, Entrada_B, Entrada_C = logic_gate_and(1)
+if Entrada_A == Entrada_B == Entrada_C: 
     print('Saida: 1')
 else: print('Saida: 0')
+
+
+# Logic gate OR
+
+# Composto por uma porta lógica, com simbolo GENERALIZADO similar a um D, porém com as duas pontas curvas 
+# para esquerda parecido com um formado de V curvo. Qual terá valores de entrada  indicando valor de 1 ou 0,
+# quando ALGUM for positivo, terá resultado de saída positivo (1) por ex:
+
+def logic_gate_or (x):
+    return x, x, x+1
+
+Entrada_A, Entrada_B, Entrada_C = logic_gate_or(1)
+if Entrada_A > 1 or Entrada_B > 1 or Entrada_C > 1: 
+    print('Saida: 1')
+else: print('Saida: 0')
+
+# Logic gate NOT/Nand
+
+# Composto por uma porta lógica AND, em que se é aplicado a lógica 'NOT', Qual será um simbolo circular em 
+# alguma das entradas ou da saida da porta, fazendo com que assim o valor da mesma entrada ou saída se inverta.
+
+def logic_gate_and(x):
+    return x, x, x
+
+Entrada_A, Entrada_B, Entrada_C = logic_gate_and(1)
+if Entrada_A == Entrada_B == Entrada_C: 
+    print('Saida: 1')
+else: print('Saida: 0')
+
